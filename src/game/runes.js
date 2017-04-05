736b84f6dd40e7dfdad3c3b9474b60fb519769e1
Yak Jun Xiang <snowypowers@gmail.com>
2017-04-05 22:27:52 +0800

    Add rune library

---
+++ src/game/runes.js
@@ -0,0 +1,44 @@
+
+/* Rune
+{
+  name    : Name of the rune
+  desc    : Short description of rune
+  func    : The function of the rune
+  priority: Priority num. Affects order of rune func
+  cost    : Cost of using the rune
+}
+*/
+const library = {
+  '0000001818000000': {
+    name: 'Do',
+    desc: 'The simplest rune you can find and make.',
+    func: (val) => {
+      return val + 1
+    },
+    priority: 0,
+    cost: 0
+  },
+  '00003c3c3c3c0000': {
+    name: 'Co',
+    desc: 'A bigger Do.',
+    func: (val) => {
+      return val + 5
+    },
+    priority: 0,
+    cost: 1
+  },
+  '0000183c3c180000': {
+    name: 'Plus',
+    desc: '1 plus 1 is 3.',
+    func: (val) => {
+
+    }
+  },
+  'undiscovered': {
+    name: '?',
+    desc: 'Undiscovered',
+    func: (val) => val
+  }
+}
+
+export default library
