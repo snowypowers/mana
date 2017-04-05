de385fb066b5f39cda92d259f9d0506ddef8837a
Yak Jun Xiang <snowypowers@gmail.com>
2017-04-05 22:29:02 +0800

    add library to state

--- src/store/state.js
+++ src/store/state.js
@@ -1,4 +1,8 @@
 
+import runes from '../game/runes.js'
+
+var library = {}
+for (var k in runes) library[k] = false
 const state = {
   mana: 0,
   runes:{
@@ -6,7 +10,8 @@ const state = {
     b: "0000000000000000",
     c: "0000000000000000",
     d: "0000000000000000"
-  }
+  },
+  library
 }
 
 export default state
